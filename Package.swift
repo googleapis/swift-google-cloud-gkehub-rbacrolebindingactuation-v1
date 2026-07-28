91ecbf02e78ff05f8a75a699706b344829218cd1
Carlos O'Ryan <coryan@google.com>
2026-07-28 17:35:15 -0400

    chore!: rename libraries used as deps (#60)
    In this pass, I renamed all the libraries that are used as dependencies
    of other libraries (e.g. `GoogleIdentityAccesscontextManagerV1`) to use
    `PascalCase` for each word.
    
    That affects a lot of the generated libraries, as `GoogleIAMV1` is used
    in many of them.
    
    
    Towards googleapis/librarian#6229

--- Package.swift
+++ Package.swift
@@ -18,19 +18,19 @@
 import PackageDescription
 
 let package = Package(
-  name: "GoogleCloudGkehubRbacrolebindingactuationV1",
+  name: "GoogleCloudGKEHubRBACRoleBindingActuationV1",
   platforms: [.macOS(.v15)],
   products: [
     .library(
-      name: "GoogleCloudGkehubRbacrolebindingactuationV1",
-      targets: ["GoogleCloudGkehubRbacrolebindingactuationV1"])
+      name: "GoogleCloudGKEHubRBACRoleBindingActuationV1",
+      targets: ["GoogleCloudGKEHubRBACRoleBindingActuationV1"])
   ],
   dependencies: [
     .package(path: "../../packages/wkt")
   ],
   targets: [
     .target(
-      name: "GoogleCloudGkehubRbacrolebindingactuationV1",
+      name: "GoogleCloudGKEHubRBACRoleBindingActuationV1",
       dependencies: [
         .product(name: "GoogleCloudWkt", package: "wkt")
       ],
