4585715fe322f31a7dd85f6652610e46b0633dd6
Jeff Ching <chingor@google.com>
2026-09-16 15:23:05 +0000

    feat(generated): update generator with support for unknown keys (#904)
    Adds support for unknown keys
    
    Fixes #447

--- Sources/GoogleCloudGKEHubRBACRoleBindingActuationV1/FeatureSpec.swift
+++ Sources/GoogleCloudGKEHubRBACRoleBindingActuationV1/FeatureSpec.swift
@@ -27,6 +27,8 @@ public struct FeatureSpec: Codable, Equatable, GoogleCloudWKT._AnyPackable,
   /// ClusterRole in this list is in use, it cannot be removed from the list.
   public var allowedCustomRoles: [Swift.String] = []
 
+  @_spi(GoogleCloudInternal) public var _unknownFields: GoogleCloudWKT._UnknownFields = .init()
+
   /// Initialize a new instance of `FeatureSpec`.
   public init() {}
 
@@ -41,6 +43,38 @@ public struct FeatureSpec: Codable, Equatable, GoogleCloudWKT._AnyPackable,
     var copy = self
     try config(&copy)
     return copy
+  }
+
+  private struct CodingKeys: CodingKey {
+    var stringValue: Swift.String
+    var intValue: Swift.Int? { nil }
+    init(stringValue: Swift.String) { self.stringValue = stringValue }
+    init?(intValue: Swift.Int) { nil }
+
+    static let allowedCustomRoles = CodingKeys(stringValue: "allowedCustomRoles")
+
+    static let _knownKeys: Set<Swift.String> = [
+      "allowedCustomRoles"
+    ]
+  }
+
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    if let value = try container.decodeIfPresent([Swift.String].self, forKey: .allowedCustomRoles) {
+      self.allowedCustomRoles = value
+    }
+    for key in container.allKeys where !CodingKeys._knownKeys.contains(key.stringValue) {
+      self._unknownFields.json[key.stringValue] = try container.decode(
+        GoogleCloudWKT.Value.self, forKey: key)
+    }
+  }
+
+  public func encode(to encoder: Encoder) throws {
+    var container = encoder.container(keyedBy: CodingKeys.self)
+    try container.encode(self.allowedCustomRoles, forKey: .allowedCustomRoles)
+    for (key, value) in self._unknownFields.json {
+      try container.encode(value, forKey: CodingKeys(stringValue: key))
+    }
   }
 
   public static var _anyTypeUrl: Swift.String {
