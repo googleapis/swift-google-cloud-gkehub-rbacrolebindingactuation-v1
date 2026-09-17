0dc61833174958ad8099412d6578d1d70c481a78
Jeff Ching <chingor@google.com>
2026-09-17 11:23:58 +0000

    refactor(gax,wkt)!: rename GoogleCloudGax and GoogleCloudWKT to GoogleGax and GoogleWKT (#924)

--- Package.swift
+++ Package.swift
@@ -32,7 +32,7 @@ let package = Package(
     .target(
       name: "GoogleCloudGKEHubRBACRoleBindingActuationV1",
       dependencies: [
-        .product(name: "GoogleCloudWKT", package: "swift-google-wkt")
+        .product(name: "GoogleWKT", package: "swift-google-wkt")
       ],
     )
   ]
